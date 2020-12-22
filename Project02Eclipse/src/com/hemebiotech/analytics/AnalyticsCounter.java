744e2cd4cdbce893ff4fd5032ddf8f6959a6b0e4
Kevnake <f13ick5@gmail.com>
2020-12-22 20:05:54 +0100

    Modification projet AnalyticsCounter.java
    -Suppression des duplications de variable (headCount/headache count)
    -MaJ path du fichier
    -Utilisation d'une boucle for à la place d'une boucle while et
    utilisation d'un tableau pour le traitement des données
    -Correction variable headCount/headacheCount

--- Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
@@ -1,41 +1,42 @@
 package com.hemebiotech.analytics;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
 import java.io.FileWriter;
+import java.util.ArrayList;
+import java.util.List;
 
 public class AnalyticsCounter {
-	private static int headacheCount = 0;	// initialize to 0
-	private static int rashCount = 0;		// initialize to 0
-	private static int pupilCount = 0;		// initialize to 0
-	
+	private static int headCount = 0; // headache counter
+	private static int rashCount = 0; // rash counter
+	private static int pupilCount = 0; // pupil counter
+
 	public static void main(String args[]) throws Exception {
 		// first get input
-		BufferedReader reader = new BufferedReader (new FileReader("symptoms.txt"));
-		String line = reader.readLine();
+		String filePath = "Project02Eclipse\\symptoms.txt";
+		ReadSymptomDataFromFile file = new ReadSymptomDataFromFile(filePath);
 
-		int i = 0;	// set i to 0
-		int headCount = 0;	// counts headaches
-		while (line != null) {
-			i++;	// increment i
-			System.out.println("symptom from file: " + line);
-			if (line.equals("headache")) {
+		// BufferedReader reader = new BufferedReader(new FileReader("symptoms.txt"));
+		// String line = reader.readLine();
+
+		List<String> symptoms = new ArrayList<>();
+		symptoms = file.GetSymptoms();
+
+		int i = 0; // set i to 0
+		for (i = 0; i < symptoms.size(); i++) {
+			System.out.println("symptom from file: " + symptoms.get(i));
+			if (symptoms.get(i).equals("headache")) {
 				headCount++;
 				System.out.println("number of headaches: " + headCount);
-			}
-			else if (line.equals("rush")) {
+			} else if (symptoms.get(i).equals("rash")) {
 				rashCount++;
-			}
-			else if (line.contains("pupils")) {
+				System.out.println("number of rash: " + rashCount);
+			} else if (symptoms.get(i).contains("pupils")) {
 				pupilCount++;
+				System.out.println("number of pupil: " + pupilCount);
 			}
-
-			line = reader.readLine();	// get another symptom
 		}
-		
-		// next generate output
-		FileWriter writer = new FileWriter ("result.out");
-		writer.write("headache: " + headacheCount + "\n");
+
+		FileWriter writer = new FileWriter("result.out");
+		writer.write("headache: " + headCount + "\n");
 		writer.write("rash: " + rashCount + "\n");
 		writer.write("dialated pupils: " + pupilCount + "\n");
 		writer.close();
